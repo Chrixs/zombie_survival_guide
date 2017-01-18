f7e61f8a28da0f06ae515d93b1fd2d2c9479986c
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-18 17:14:30 +0000

    form formatted a bit

--- interface.js
+++ interface.js
@@ -5,9 +5,12 @@ $("#button").click(function(e) {
       var p = new PersonalizedResponse();
       var survivalPercentage = p.survivalPercentage(p.getTotalScore(p.getValue($('.question1').serializeArray())));
         $('.form-response').html(
-          "<div id=survival-chance>Chance of survival: " + survivalPercentage + "</div><br>" +
-          "<p><a href='https://mapofthedead.com/'>Click here</a> for all your google maps zombie needs</p><br>" +
-          "<p><a href='./survivalPlan.html'>Survival Plan for the end of the world</a></p>"
+          "<div class=response-parallax></div><br/>" +
+          "<div id=survival-chance>Chance of survival: " + survivalPercentage + "<br/>" +
+          "<p><a href='https://mapofthedead.com/'>Click here</a> for all your google maps zombie needs</p><br/>" +
+          "<p>We recommend putting a survival plan in place, here is one you can reference: " +
+          "<a href='./survivalPlan.html'>Survival Plan for the end of the world</a></p></div><br/>" +
+          "<div class=response-parallax></div>"
         );
     };
     update();
